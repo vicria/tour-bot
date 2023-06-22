3775e54de04ad85ce0fe9b2f5dc772d415fa8ec4
abishkam <mollaev6@mail.ru>
2023-06-22 12:19:48 +0300

    task-3:Локализовать сообщения

--- telegram/telegram-microservice/src/main/java/ar/vicria/telegram/microservice/rb/Messages.java
+++ telegram/telegram-microservice/src/main/java/ar/vicria/telegram/microservice/rb/Messages.java
@@ -8,37 +8,102 @@ import java.util.Locale;
 public class Messages {
 
     private final MessageSource ms = new MessageSource();
+    /**
+     * current locale.
+     */
     private final Locale locale;
 
+    /**
+     * time from AnswerDerailsQuery.
+     */
     private final String adqTime;
+    /**
+     * distance from AnswerDerailsQuery.
+     */
     private final String adqDistance;
+    /**
+     * hide from AnswerDerailsQuery.
+     */
     private final String adqHide;
-
+    /**
+     * time from AnswerQuery.
+     */
     private final String aqTime;
+    /**
+     * moredetailed from AnswerQuery.
+     */
     private final String aqMoredetailed;
-
+    /**
+     * selectbranch from BranchQuery.
+     */
     private final String bqSelectbranch;
+    /**
+     * route from BranchQuery.
+     */
     private final String bqRoute;
-
+    /**
+     * selectstation from StationQuery.
+     */
     private final String sqSelectstation;
+    /**
+     * "from" from StationQuery.
+     */
     private final String sqFrom;
-
+    /**
+     * selectDirection from RoutMessage.
+     */
     private final String rmessageSelectDirection;
+    /**
+     * rout from RoutMessage.
+     */
     private final String rmessageRoute;
+    /**
+     * "from" from RoutMessage.
+     */
     private final String rmessageFrom;
+    /**
+     * "to" from RoutMessage.
+     */
     private final String rmessageTo;
-
+    /**
+     * menuSubte from StartMessage.
+     */
     private final String smMenuSubte;
+    /**
+     * route from StartMessage.
+     */
     private final String smRoute;
+    /**
+     * feedback from StartMessage.
+     */
     private final String smFeedback;
+    /**
+     * aboutCapabilities from StartMessage.
+     */
     private final String smAboutCapabilities;
-
+    /**
+     * "from" from RoutMsg.
+     */
     private final String rmsgFrom;
+    /**
+     * "to" from RoutMsg.
+     */
     private final String rmsgTo;
+    /**
+     * select from RoutMsg.
+     */
     private final String rmsgSelect;
+    /**
+     * willTake from RoutMsg.
+     */
     private final String rmsgWillTake;
+    /**
+     * route from RoutMsg.
+     */
     private final String rmsgRoute;
-
+    /**
+     * selectItem from TelegramConnector.
+     */
     private final String tgcSelectItem;
 
     public Messages(Locale locale) {
