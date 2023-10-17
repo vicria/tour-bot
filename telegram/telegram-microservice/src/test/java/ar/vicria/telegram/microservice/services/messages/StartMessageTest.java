d795c507f42f4b418dcc5de8e0b8cc5eec062796
Arseny Ptitsyn <senyabird@yandex.ru>
2023-10-17 19:37:25 +0300

    Initial commit.

---
+++ telegram/telegram-microservice/src/test/java/ar/vicria/telegram/microservice/services/messages/StartMessageTest.java
@@ -0,0 +1,24 @@
+package ar.vicria.telegram.microservice.services.messages;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartMessageTest {
+
+    @Test
+    void supports() {
+    }
+
+    @Test
+    void process() {
+    }
+
+    @Test
+    void question() {
+    }
+
+    @Test
+    void answer() {
+    }
+}
