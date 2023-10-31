a3ac1a8103a37e3f67990e1324686481f5483241
Arseny Ptitsyn <senyabird@yandex.ru>
2023-10-31 12:44:02 +0300

    Did a parametrized test for LocalizedTelegramMessage.

--- telegram/telegram-microservice/src/test/java/ar/vicria/telegram/microservice/localizations/LocalizedTelegramMessageTest.java
+++ telegram/telegram-microservice/src/test/java/ar/vicria/telegram/microservice/localizations/LocalizedTelegramMessageTest.java
@@ -1,28 +1,21 @@
 package ar.vicria.telegram.microservice.localizations;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LocalizedTelegramMessageTest {
 
-    @Test
-    void getCommon1() {
-        String common = LocalizedTelegramMessage.getCommon(
-                "\\nВыберите ветку",
-                "Выберите направление"
-        );
-
-        assertEquals("Выберите", common);
-    }
-
-    @Test
-    void getCommon2() {
-        String common = LocalizedTelegramMessage.getCommon(
-                "\\n Выберите ветку",
-                "Поменяйте направление"
-        );
-
-        assertEquals("", common);
+    @ParameterizedTest
+    @CsvSource(value = {
+            "\\nВыберите ветку   |   \\nВыберите направление   |   Выберите     ",
+            "\\nВыберите ветку   |   \\n                       |   ''           ",
+    }, delimiter = '|')
+    void getCommon(String string1, String string2, String expectedCommon) {
+        String common = LocalizedTelegramMessage.getCommon(string1, string2);
+        assertEquals(expectedCommon, common);
     }
 }
