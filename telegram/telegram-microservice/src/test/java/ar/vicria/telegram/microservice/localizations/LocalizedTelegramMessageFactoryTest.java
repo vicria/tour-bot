70f61540bcbdd8eb8567f4cc858d154b1be53728
vicria <vicria@inbox.ru>
2023-12-07 17:53:30 -0300

    revert

--- telegram/telegram-microservice/src/test/java/ar/vicria/telegram/microservice/localizations/LocalizedTelegramMessageFactoryTest.java
+++ telegram/telegram-microservice/src/test/java/ar/vicria/telegram/microservice/localizations/LocalizedTelegramMessageFactoryTest.java
@@ -15,7 +15,7 @@ class LocalizedTelegramMessageFactoryTest {
     @CsvSource(value = {
             "from        |   en  ",
             "до          |   ru  ",
-            "desde       |   es2  ",
+            "desde       |   es  ",
     }, delimiter = '|')
     void getLocalizedByWord(String msg, String lang) {
         var factory = new LocalizedTelegramMessageFactory();
